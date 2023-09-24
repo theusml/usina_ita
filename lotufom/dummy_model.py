46511d58b490161528a46402ce7f9333a1460f99
Matheus Lotufo <matheusmlotufo@usp.br>
2023-09-24 00:02:27 -0300

    ajuste no modelo e bases em parquet

--- lotufom/dummy_model.py
+++ lotufom/dummy_model.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 
 #%%
 # Leitura dos dados
-dados = pd.read_excel(r'..\dados\BIMTRA_train.xlsx')
+dados = pd.read_parquet(r'..\dados\BIMTRA_train.parquet')
 
 #%%
 # Removendo outliers
@@ -24,15 +24,15 @@ dados = dados.reset_index(drop=True)
 
 dados['tempo'] = dados.dt_arr - dados.dt_dep
 
-scalar = MinMaxScaler()
-scalar = MinMaxScaler()
+scaler = MinMaxScaler()
+scaler = MinMaxScaler()
 
-dados['tempo'] = scalar.fit_transform(dados[['tempo']].values)
+dados['tempo'] = scaler.fit_transform(dados[['tempo']].values)
 
 # %%
 # Scorando o modelo
 
-splits = 25
+splits = 50
 kf = KFold(splits, shuffle=True, random_state=200)
 
 results = []
@@ -43,7 +43,7 @@ for train_index, test_index in tqdm(kf.split(dados), total=splits):
 
     modelo = smf.ols(formula='tempo ~ C(origem) : C(destino)', data=data_train).fit()
 
-    y_pred = modelo.predict(sm.add_constant(data_test))
+    y_pred = modelo.predict(data_test)
 
     results.append(r2_score(y_true, y_pred))
 
@@ -52,6 +52,18 @@ alpha = np.std(results)
 print(f'Model Score: {round(u, 4)} ± {round(1.96*alpha, 3)}')
 
 # %%
+# Final dummy model
+dummy_model = smf.ols(formula='tempo ~ C(origem) : C(destino)', data=dados).fit()
+y_pred = dummy_model.predict(dados)
 
+#%%
+# Capturando os maiores ofensores
+dados['tempo_pred'] = y_pred
+dados['tempo_diff'] = abs(dados.tempo_pred - dados.tempo)
+
+dados.to_parquet(r'dados_dummy_model.parquet', index=False)
+
+dados_piores_scores = dados.sort_values(
+'tempo_diff', ascending=False).head(20)
+dados_piores_scores.to_parquet('piores_scores_modelo_dummy.parquet', index=False)
 # %%
-
